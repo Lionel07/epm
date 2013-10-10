fa3863b884d822d7876f362197bab118a9e08db5
Corwin Lee McKnight <lioneltabre@gmail.com>
2013-10-10 14:08:12 -0700

    "Started Main"

---
+++ main.c
@@ -0,0 +1,14 @@
+//Elemental Package Manager: main.c
+//by Corwin McKnight
+
+#include <stdio.h>
+
+int main()
+{
+	//TODO: GET OPTIONS
+	//TODO: INTERPET OPTIONS
+	//TODO: GET PACKAGE
+	//TODO: READ PACKAGE
+	//TODO: DO STUFF TO PACKAGE
+	//TODO: YEAHHHHH
+}
