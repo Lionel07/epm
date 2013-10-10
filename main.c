923bcca14d7a6ac669f6ac661a2e43dce78ec71d
Corwin Lee McKnight <lioneltabre@gmail.com>
2013-10-10 15:00:58 -0700

    Added Makefile; started main

--- main.c
+++ main.c
@@ -3,9 +3,26 @@
 
 #include <stdio.h>
 
-int main()
+void printHelp(char* usage_exename)
+{
+	printf("Elemental Package Manager (epm)\n");
+	printf("\tUsage: %s [command] {arguments}\n",usage_exename);
+	printf("Avalable Commands:\n");
+	printf("install: Installs Packages\n");
+	printf("get:\t Retrieves Packages\n");
+	printf("update:\t Updates Packages\n");
+	printf("upgrade: Upgrades Packages\n");
+	printf("remove:\t Removes Packages\n");
+}
+
+int main(int argc, char **argv)
 {
 	//TODO: GET OPTIONS
+	int realargc=argc-1;
+	if(!realargc)
+	{
+		printHelp(argv[0]);
+	}
 	//TODO: INTERPET OPTIONS
 	//TODO: GET PACKAGE
 	//TODO: READ PACKAGE
